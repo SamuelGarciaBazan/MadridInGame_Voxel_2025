5bfd6990d2d8bb1b73d1f3ac2b13b06e434ef41e
SamuelGarciaBazan <samuga05@ucm.es>
2025-05-10 15:33:22 +0200

    solo hover en el mas cercano

--- FentaVoxel/Assets/Scripts/Player/PlayerWork.cs
+++ FentaVoxel/Assets/Scripts/Player/PlayerWork.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerWork : MonoBehaviour
@@ -13,23 +14,106 @@ public class PlayerWork : MonoBehaviour
 
     [SerializeField]    
     float outLineOffHover;
-    
+
+
+    float _nearestDistance;
+
+    GameObject _lastNearest;
+
+
+    private List<GameObject> _hoveredResources = new List<GameObject>();
+    private void FixedUpdate()
+    {
+        //_lastNearest = null;    
+        //_nearestDistance = Mathf.Infinity;
+
+        if (_hoveredResources.Count == 0)
+            return;
+
+        GameObject nearest = null;
+        float nearestDist = float.MaxValue;
+
+        foreach (var obj in _hoveredResources)
+        {
+            if (obj == null) continue;
+
+            float dist = Vector3.Distance(transform.position, obj.transform.position);
+            if (dist < nearestDist)
+            {
+                nearestDist = dist;
+                nearest = obj;
+            }
+        }
+
+        foreach (var obj in _hoveredResources)
+        {
+            if (obj == null) continue;
+
+            var outline = obj.GetComponentInChildren<Outline>();
+            if (outline == null) continue;
+
+            outline.setOutlineWidht(obj == nearest ? outLineOnHover : outLineOffHover);
+        }
+
+        _lastNearest = nearest;
+
+        // Limpia la lista para el siguiente frame
+        _hoveredResources.Clear();
+
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        print(other.gameObject); 
-
+        return;
 
         //si es un recurso
         if ( ((1 << other.gameObject.layer) & _resourcesMask.value) != 0)
         {
-            var outline = other.GetComponentInChildren<Outline>();
 
-           
-            outline.setOutlineWidht(outLineOnHover);
+            float distance = Vector3.Distance(transform.position, other.transform.position);
+
+            if (distance < _nearestDistance) { 
+            
+                _nearestDistance = distance;
+
+                if (_lastNearest != null) {
+                    
+                    var lastOutline = _lastNearest.GetComponentInChildren<Outline>();
+
+
+                    lastOutline.setOutlineWidht(outLineOffHover);
+
+                }
+
+                _lastNearest = other.gameObject;
+
+                var outline = other.GetComponentInChildren<Outline>();
+
+
+                outline.setOutlineWidht(outLineOnHover);
+
+            }
+
+
+ 
         }
-
-
     }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //si es un recurso
+        if (((1 << other.gameObject.layer) & _resourcesMask.value) != 0)
+        {
+            if (!_hoveredResources.Contains(other.gameObject))
+            {
+                _hoveredResources.Add(other.gameObject);
+            }
+                
+
+        }
+    }
+
+
 
 
     private void OnTriggerExit(Collider other)
