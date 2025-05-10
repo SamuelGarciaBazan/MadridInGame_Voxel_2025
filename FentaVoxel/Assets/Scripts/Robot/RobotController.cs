47f1174155420d47398f38be3957caf933474806
SamuelGarciaBazan <samuga05@ucm.es>
2025-05-10 16:32:01 +0200

    fixsssssssss

--- FentaVoxel/Assets/Scripts/Robot/RobotController.cs
+++ FentaVoxel/Assets/Scripts/Robot/RobotController.cs
@@ -52,6 +52,7 @@ public class RobotController : MonoBehaviour
             if(newTarget == RobotManager.RobotResourceTarget.NONE)
             {
                 _currentState = RobotState.DESACTIVE;
+                _navMeshAgent.SetDestination(transform.position);
             }
             else
             {
